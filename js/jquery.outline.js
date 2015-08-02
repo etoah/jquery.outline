b06f0eb423e1866173e33aba355b3cf12584f681
etoah <etoahn@163.com>
2015-08-02 11:14:34 +0800

    实现全部功能

--- js/jquery.outline.js
+++ js/jquery.outline.js
@@ -1,73 +1,174 @@
-(function($){
+/*!
+ * jQuery Scrollspy Plugin
+ * Author: @sxalexander，Lucien
+ * Licensed under the MIT license
+ */
+;
+(function ($, window, document, undefined) {
+
+    $.fn.extend({
+        scrollspy: function (options) {
+
+            var defaults = {
+                min: 0,
+                max: 0,
+                mode: 'vertical',
+                namespace: 'scrollspy',
+                buffer: 0,
+                container: window,
+                onEnter: [],
+                onLeave: [],
+                onTick: []
+            }
+
+            var options = $.extend({}, defaults, options);
+
+            return this.each(function (i) {
+
+                var element = this;
+                var o = options;
+                var $container = $(o.container);
+                var mode = o.mode;
+                var buffer = o.buffer;
+                var enters = leaves = 0;
+                var inside = false;
+
+                /* add listener to container */
+                $container.bind('scroll.' + o.namespace, function (e) {
+                    var position = {top: $(this).scrollTop(), left: $(this).scrollLeft()};
+                    var xy = (mode == 'vertical') ? position.top + buffer : position.left + buffer;
+                    var max = o.max;
+                    var min = o.min;
+
+                    /* fix max */
+                    if ($.isFunction(o.max)) {
+                        max = o.max();
+                    }
+
+                    /* fix max */
+                    if ($.isFunction(o.min)) {
+                        min = o.min();
+                    }
+
+                    if (max == 0) {
+                        max = (mode == 'vertical') ? $container.height() : $container.outerWidth() + $(element).outerWidth();
+                    }
+
+                    /* if we have reached the minimum bound but are below the max ... */
+                    if (xy >= min && xy <= max) {
+                        /* trigger enter event */
+                        if (!inside) {
+                            inside = true;
+                            enters++;
+
+                            /* fire enter event */
+                            $(element).trigger('scrollEnter', {position: position})
+                            if ($.isFunction(o.onEnter)) {
+                                o.onEnter(element, position);
+                            }
+
+                        }
+
+                        /* trigger tick event */
+                        $(element).trigger('scrollTick', {
+                            position: position,
+                            inside: inside,
+                            enters: enters,
+                            leaves: leaves
+                        })
+                        if ($.isFunction(o.onTick)) {
+                            o.onTick(element, position, inside, enters, leaves);
+                        }
+                    } else {
+
+                        if (inside) {
+                            inside = false;
+                            leaves++;
+                            /* trigger leave event */
+                            $(element).trigger('scrollLeave', {position: position, leaves: leaves})
+
+                            if ($.isFunction(o.onLeave)) {
+                                o.onLeave(element, position);
+                            }
+                        }
+                    }
+                });
+
+            });
+        }
+
+    })
+})(jQuery, window, document, undefined);
+
+
+/*!
+ * jQuery outline Plugin
+ * Author: @Lucien  etoahn@163.com
+ */
+
+
+;
+(function ($) {
     "use strict";
-    function createLink(idSuffix,text)
-    {
-        var link= document.createElement('a');
-        link.href='#outline_'+idSuffix;
-        link.innerHTML=text;
+    function createLink(idSuffix, text) {
+        var link = document.createElement('a');
+        link.href = '#outline_' + idSuffix;
+        link.innerHTML = text;
         return link;
     }
 
-    function createHeader(wrapDiv,element)
-    {
+    function createHeader(wrapDiv, element) {
         var header = document.createElement("h3");
-        header.innerHTML=element.innerHTML;
+        header.innerHTML = element.innerHTML;
         wrapDiv.appendChild(header);
     }
 
-    function createOutline(id,elements,hasTitle)
-    {
-        var i=0;//index of elements
-        var content=$(id);
-        var wrapDiv = content[0]||content;
-        hasTitle&&createHeader(wrapDiv,elements[i++]);
+    function createOutline(id, elements, hasTitle) {
+        var i = 0;//index of elements
+        var content = $(id);
+        var wrapDiv = content[0] || content;
+        hasTitle && createHeader(wrapDiv, elements[i++]);
 
         var ul = document.createElement("ul");
-        var len=elements.length;
+        var len = elements.length;
 
-        var li=document.createElement('li');
-        var link= createLink(i,elements[i++].innerHTML);
+        var li = document.createElement('li');
+        var link = createLink(i, elements[i++].innerHTML);
         li.appendChild(link);
         ul.appendChild(li); //create first li element to compare
 
-        for (; i < len; i++){
+        for (; i < len; i++) {
 
-            li= createElement(li,elements[i],elements[i-1].nodeName,i)
+            li = createElement(li, elements[i], elements[i - 1].nodeName, i)
         }
 
         wrapDiv.appendChild(ul);
         return wrapDiv;
     }
 
-    function createElement(current,element,lastNodeName,index)
-    {
-        var link= createLink(index,element.innerHTML);
+    function createElement(current, element, lastNodeName, index) {
+        var link = createLink(index, element.innerHTML);
         var ul,
-            li=document.createElement('li');
+            li = document.createElement('li');
         li.appendChild(link);
-        if(element.nodeName>lastNodeName)//eg. H3>H2
+        if (element.nodeName > lastNodeName)//eg. H3>H2
         {
             ul = document.createElement("ul");
             ul.appendChild(li);
             current.appendChild(ul);
         }
-        else if(element.nodeName===lastNodeName)
-        {
+        else if (element.nodeName === lastNodeName) {
             current.parentNode.appendChild(li);
         }
-        else
-        {
+        else {
             //当为ul时继续查找
-            while(current.nodeName.toUpperCase()==='UL'||(document.getElementById(current.firstChild.getAttribute('href').substring(1)).nodeName!==element.nodeName))
-            {
-                if(current.parentNode)
-                {
-                    current=current.parentNode;
+            while (current.nodeName.toUpperCase() === 'UL' || (document.getElementById(current.firstChild.getAttribute('href').substring(1)).nodeName !== element.nodeName)) {
+                if (current.parentNode) {
+                    current = current.parentNode;
                 }
-                else
-                {
+                else {
                     //如果没有父节点，说明是顶层的ul，把当前元素置为顶层ul第一个子元素,方便插入
-                    current=current.firstChild;
+                    current = current.firstChild;
                     break;
                 }
 
@@ -80,89 +181,79 @@
 
     }
 
-    function validate(option)
-    {
-        if(!option.contentSelector)
-        {
-             throw new TypeError("contentSelector is Required attribute");
+    function validate(option) {
+        if (!option.contentSelector) {
+            throw new TypeError("contentSelector is Required attribute");
         }
     }
 
 
-    var ol={
-        outline:function(option)
-        {
+    var ol = {
+        outline: function (option) {
 
             validate(option);
 
-            var option= $.extend({
-                selector:'body',  //css选择器，选择的范围
-                hArray:['h1','h2','h3','h4','h5','h6'], //要识别的header
-                addSpy:true,
-                hasTitle:false, //范围内是否第一个是标题
-                offset:20
-            },option);
+            var option = $.extend({
+                selector: 'body',  //css选择器，选择的范围
+                hArray: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'], //要识别的header
+                addSpy: true,//是否添加滚动事件监听
+                hasTitle: false, //范围内是否第一个是标题
+                offset: 20 //偏离的位置
+            }, option);
 
-            var hSelector=option.hArray.join(',');
-            var index=0;
-            $(option.selector).find(hSelector).each(function()
-            {
-                var id='outline_'+index++;
-                this.setAttribute('id',id);
+            var hSelector = option.hArray.join(',');
+            var index = 0;
 
-                if(option.addSpy){
+            //先生成outline,再在下一步添加监听
+            $(option.selector).find(hSelector).each(function () {
+                var id = 'outline_' + index++;
+                this.setAttribute('id', id);
+            });
+
+            createOutline(option.contentSelector, $(option.selector).find(hSelector), option.hasTitle);
+            if (option.addSpy) {
+                $(option.selector).find(hSelector).each(function () {
+
                     //scrollspy
-                    var $that=$(this);
+                    var $that = $(this);
                     var position = $that.position();
-                    console.log(this);
                     $that.scrollspy({
 
-                        min:  $that.offset().top-option.offset,
+                        min: $that.offset().top - option.offset,
                         max: (function () {
-                            var next= $that.nextAll(hSelector)[0];//下一个标题
+                            var next = $that.nextAll(hSelector)[0];//下一个标题
 
-                            return next?$(next).offset().top:$that.offset().top;
+                            return next ? $(next).offset().top : $that.offset().top;
                         })(),
-                        onEnter: function(element, position) {
-
-
-                            $('[href="#'+element.id+'"]').addClass("active-header");
+                        onEnter: function (element, position) {
+                            $('[href="#' + element.id + '"]').addClass("active-header");
                         },
-                        onLeave: function(element, position) {
-                            $('[href="#'+element.id+'"]').removeClass("active-header");
+                        onLeave: function (element, position) {
+                            $('[href="#' + element.id + '"]').removeClass("active-header");
                         }
                     });
-                }
-            });
-            createOutline(option.contentSelector,$(option.selector).find(hSelector),option.hasTitle)
-
+                });
+            }
 
         }
 
     };
 
 
-
-
-
     $.extend(ol);
-
 
 
     /*data API*/
 
-    if($('[data-outline]'))
-    {
+    if ($('[data-outline]')) {
 
-        $.outline({contentSelector:'[data-outline]',selector:$('[data-outline]').data('outline')});
+        $.outline({contentSelector: '[data-outline]', selector: $('[data-outline]').data('outline')});
 
     }
 
 
-
-
-
-})(jQuery);/**
+})(jQuery);
+/**
  * Created by Lucien on 7/14/2015.
  */
 
